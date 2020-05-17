59e845794576124e9a5970f693924f71d1a3e9f9
ToanLV <toanleviet95@gmail.com>
2020-05-18 00:29:39 +0700

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    Tech Stack:
+    <ul>
+      <li>React</li>
+      <li>React Hot Loader</li>
+      <li>Babel</li>
+      <li>Webpack</li>
+      <li>ESlint</li>
+      <li>Stylelint</li>
+      <li>Travis CI</li>
+    </ul>
+  </div>
+);
+
+export default App;
